73ddeb4cf980e40ca290152095cfc997b7019cd6
Aaron Addleman <aaronaddleman@gmail.com>
2015-07-21 08:44:45 -0700

    testing attribute levels

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,9 @@
 # All rights reserved - Do Not Redistribute
 #
 
+node.default['attributes']['lvl_02'] = "Default value from: lvl_2 recipe file"
+
+
 template '/tmp/example_of_precedence' do
   source 'examples.erb'
 end
